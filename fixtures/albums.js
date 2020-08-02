56039731439f46342aecf1f34d0a5e15fc6534ed
SeaFalcon <seafalcon323@gmail.com>
2020-08-03 01:54:59 +0900

    generate mock data
    - make fixture folder and album infomation

---
+++ fixtures/albums.js
@@ -0,0 +1,22 @@
+const albums = [
+  {
+    artist: 'Septicflesh',
+    album: 'Infernus Sinfonica MMXIX  [Live]',
+    rating: '96.1',
+    rank: 1,
+    vote: 9,
+    reviews: 0,
+    releaseDate: 'Jul 31, 2020',
+  },
+  {
+    artist: 'H.E.A.T',
+    album: 'H.E.A.T II',
+    rating: '92.2',
+    rank: 2,
+    vote: 20,
+    reviews: 0,
+    releaseDate: 'Feb 21, 2020',
+  },
+];
+
+export default albums;
