3618ae8f2c0b038029b684680fe6e270231ebe81
SeaFalcon <seafalcon323@gmail.com>
2020-08-29 12:03:07 +0900

    add devServer / prodServer

--- src/services/api.js
+++ src/services/api.js
@@ -1,12 +1,15 @@
+const devServer = 'http://localhost:3000';
+const prodServer = 'https://heavymetal-universe.herokuapp.com/';
+
 export async function fetchAlbums({ year, page, limit } = {}) {
-  const url = `http://localhost:3000/albumoftheyear?year=${year}&page=${page}&limit=${limit}`;
+  const url = `${prodServer}/albumoftheyear?year=${year}&page=${page}&limit=${limit}`;
   const response = await fetch(url);
   const data = await response.json();
   return data;
 }
 
 export async function fetchAvailableYears() {
-  const url = 'http://localhost:3000/yearlist';
+  const url = `${prodServer}/yearlist`;
   const response = await fetch(url);
   const data = await response.json();
   return data;
